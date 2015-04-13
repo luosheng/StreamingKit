a0b85150d695730273707bc59b913deec5424981
Luo Sheng <luosheng1986@gmail.com>
2015-04-13 14:17:45 +0800

    Update public visibility.

--- StreamingKit/StreamingKit/StreamingKit.h
+++ StreamingKit/StreamingKit/StreamingKit.h
@@ -5,8 +5,7 @@
 //  Created by Luo Sheng on 4/13/15.
 //  Copyright (c) 2015 Pop Tap. All rights reserved.
 //
-
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for StreamingKit.
 FOUNDATION_EXPORT double StreamingKitVersionNumber;
@@ -15,5 +14,11 @@ FOUNDATION_EXPORT double StreamingKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char StreamingKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <StreamingKit/PublicHeader.h>
-
-
+#import <StreamingKit/STKAudioPlayer.h>
+#import <StreamingKit/STKAutoRecoveringHTTPDataSource.h>
+#import <StreamingKit/STKCoreFoundationDataSource.h>
+#import <StreamingKit/STKDataSource.h>
+#import <StreamingKit/STKDataSourceWrapper.h>
+#import <StreamingKit/STKHTTPDataSource.h>
+#import <StreamingKit/STKLocalFileDataSource.h>
+#import <StreamingKit/STKQueueEntry.h>
