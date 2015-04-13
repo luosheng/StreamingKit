06c486d74cc338649d0902c25acaec37bf4a648b
Luo Sheng <luosheng1986@gmail.com>
2015-04-13 13:44:11 +0800

    Reconstruct the project to be dynamic framework type.

---
+++ StreamingKit/StreamingKit/StreamingKit.h
@@ -0,0 +1,19 @@
+//
+//  StreamingKit.h
+//  StreamingKit
+//
+//  Created by Luo Sheng on 4/13/15.
+//  Copyright (c) 2015 Pop Tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreamingKit.
+FOUNDATION_EXPORT double StreamingKitVersionNumber;
+
+//! Project version string for StreamingKit.
+FOUNDATION_EXPORT const unsigned char StreamingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamingKit/PublicHeader.h>
+
+
